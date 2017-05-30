34f84660904c57c4f3b734f48ebd3bc6949d3e29
Ivan G. Volkov <i@volkov.work>
2017-05-30 23:40:16 +0900

    Add Init.d scripts for redis and redis-sentinel instanced groups

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""Redis sharding  tool
+
+:copyright: 2017 by Ivan G. Volkov.
+:license: BSD, see LICENSE for more details.
+"""
+
+__version__ = "0.1"
+__project__ = "redice"
+__author__ = "Ivan G. Volkov <i@volkov.work>"
+__license__ = "GNU LGPL"
